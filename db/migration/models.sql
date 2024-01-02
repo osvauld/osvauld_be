b28732789cb94f56db09105687a13d1f9490b86a
abrahamgeorge8547 <abrahamgeorge8547@gmail.com>
2024-01-02 15:56:58 +0530

    changing public key column name from public_key to rsa_public_key

--- db/migration/models.sql
+++ db/migration/models.sql
@@ -9,7 +9,7 @@ CREATE TABLE users (
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     username VARCHAR(255) UNIQUE NOT NULL,
     name VARCHAR(255) NOT NULL, 
-    public_key TEXT,
+    rsa_public_key TEXT,
     ecc_pub_key TEXT,
     temp_password VARCHAR(255) NOT NULL,
     signed_up BOOLEAN NOT NULL DEFAULT FALSE
