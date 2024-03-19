0c3f23568d58ae61e1357125d591ab8bbc2bd5e5
akash1729 <akashthomasjohn@gmail.com>
2024-03-19 22:04:26 +0530

    fmt

--- routers/middleware/cors.go
+++ routers/middleware/cors.go
@@ -6,7 +6,7 @@ import (
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		ctx.Writer.Header().Set("Access-Control-Allow-Origin",  "chrome-extension://omcgaifpkbjfpikdgimmpdkibdffinil")
+		ctx.Writer.Header().Set("Access-Control-Allow-Origin", "chrome-extension://omcgaifpkbjfpikdgimmpdkibdffinil")
 		ctx.Writer.Header().Set("Access-Control-Max-Age", "86400")
 		ctx.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
 		ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, api_key, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
