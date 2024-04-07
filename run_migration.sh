bd337e93b3074270dfd89b6ed871ca131624bcb2
akash1729 <akashthomasjohn@gmail.com>
2024-04-08 00:57:01 +0530

    migration file fix

--- run_migration.sh
+++ run_migration.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+
 db_url="postgresql://${MASTER_DB_USER}:${MASTER_DB_PASSWORD}@${MASTER_DB_HOST}:${MASTER_DB_PORT}/${MASTER_DB_NAME}?sslmode=${MASTER_SSL_MODE}"
 make migrateup DB_URL="$db_url"
