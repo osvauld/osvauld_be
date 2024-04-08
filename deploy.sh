9bd517fc3e813ca3a51070e7911a0c978648ad35
abrahamgeorge8547 <abrahamgeorge8547@gmail.com>
2024-04-08 18:08:39 +0530

    deployment using main

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,11 @@ ssh ubuntu@3.110.128.10 << 'EOF'
 
     cd osvauld_be
 
+    git checkout main
+    
     git pull
+
+    sudo docker build -t osvauld_be:latest .
 
     sudo docker stop osvauld_backend
 
@@ -14,7 +18,6 @@ ssh ubuntu@3.110.128.10 << 'EOF'
 
     sudo docker rmi osvauld_be:latest
 
-    sudo docker build -t osvauld_be:latest .
 
     sudo docker run --name osvauld_backend \
     -d \
