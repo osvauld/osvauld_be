dc9bea1d554fa65a2e0a440799a5625d40534470
abrahamgeorge8547 <abrahamgeorge8547@gmail.com>
2024-04-08 17:55:39 +0530

    test docker build time

--- deploy.sh
+++ deploy.sh
@@ -8,13 +8,13 @@ ssh ubuntu@3.110.128.10 << EOF
 
     git pull
 
+    sudo docker build -t osvauld_be:latest .
+
     sudo docker stop osvauld_backend
 
     sudo docker rm osvauld_backend
 
     sudo docker rmi osvauld_be:latest
-
-    sudo docker build -t osvauld_be:latest .
 
     sudo docker run -d -p 80:8000 --name osvauld_backend osvauld_be:latest
 EOF
