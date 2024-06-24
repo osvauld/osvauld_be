0dfca0c82cd524e89cabda020453f6986b3ea34c
abe <abe@osvauld.com>
2024-06-24 13:33:07 +0530

    delete cascade added

--- db/migration/000003_cli_user.up.sql
+++ db/migration/000003_cli_user.up.sql
@@ -3,7 +3,7 @@ ALTER TABLE users ADD COLUMN created_by UUID;
 
 CREATE TABLE environments (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    cli_user UUID NOT NULL REFERENCES users(id),
+    cli_user UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     name VARCHAR(255) NOT NULL,
     createdAt TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updatedAt TIMESTAMPTZ NOT NULL DEFAULT NOW(),
@@ -14,8 +14,8 @@ CREATE TABLE environment_fields (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     field_name VARCHAR(255) NOT NULL,
     field_value TEXT NOT NULL,
-    parent_field_value_id UUID NOT NULL REFERENCES field_values(id),
-    env_id UUID NOT NULL REFERENCES environments(Id),
+    parent_field_value_id UUID NOT NULL REFERENCES field_values(id) ON DELETE CASCADE,
+    env_id UUID NOT NULL REFERENCES environments(Id) ON DELETE CASCADE,
     credential_id UUID NOT NULL REFERENCES credentials(id) ON DELETE CASCADE,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
